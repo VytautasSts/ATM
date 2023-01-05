31a92c78cee3bd72ba615ce237861417e02b9130
VytautasSts <111770550+VytautasSts@users.noreply.github.com>
2023-01-05 11:12:29 +0200

    Project Base file upload
    Uploaded project base files to GIT

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
